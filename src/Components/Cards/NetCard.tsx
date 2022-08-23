eb103f905aa11326c6a2273b5146bd033b875e58
Павел <akumapl86@gmail.com>
2022-08-23 18:54:30 +0300

    sync

--- src/Components/Cards/NetCard.tsx
+++ src/Components/Cards/NetCard.tsx
@@ -27,26 +27,26 @@ const NetCard: React.FC<INetCardProps & { isForbidden?: boolean }> = (props) => 
     const isForbCls = forb ? "red darken-5" : "grey lighten-2"
     return (
         <div className={`form__list z-depth-3 px1 ${isForbCls}`} >
+
             <div className='flex-row'>
+                <button className={`btn    ${isSKF ? "blue accent-2" : "blue-grey darken-4"} w100 flex-row`}
+                    onClick={() => toggle()}
+                >
+                    <i className={forb ? "red-text" : "white-text"}>Ш: {calcedNet.W}  <I title='compare_arrows' /> В: {calcedNet.H} </i>
+                </button>
+                <span className='px1'>
+                    {forb && <I title='dangerous' className='red-text text-darken-4 I-wrapper white' />}
+                    {!forb && isSKF && <I title='grid_4x4' wrapped className='deep-red-text text-darken-2' />}
+                    {!forb && !isSKF && <I title='grid_3x3' wrapped className='blue-text text-darken-4' />}
+                </span>
                 <button className={`btn  pb1  z-depth-2 ${isSKF ? "deep-orange darken-2" : "blue-grey darken-4"} `}
                     style={{ width: "100px" }}
                     onClick={() => toggle()}
                 >
                     {isSKF ? "SKF" : "Простая"}
                 </button>
-
-                <span className='px1'>
-                    {forb && <I title='dangerous' className='red-text text-darken-4 I-wrapper white' />}
-                    {!forb && isSKF && <I title='grid_4x4' wrapped className='deep-red-text text-darken-2' />}
-                    {!forb && !isSKF && <I title='grid_3x3' wrapped className='blue-text text-darken-4' />}
-                </span>
-                <button className={`btn    ${isSKF ? "blue accent-2" : "blue-grey darken-4"} `}
-                    onClick={() => toggle()}
-                >
-                    <b className={forb ? "red-text" : "white-text"}>{calcedNet.W} мм x {calcedNet.H} мм</b>
-                </button>
-
             </div>
+
             <div className='flex-row'>
                 <div className='flex-col'>
                     <span className='center'> Световой проем: </span>
