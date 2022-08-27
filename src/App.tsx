d6d525990386ddbff6c13c035e75006883facfd3
Павел <akumapl86@gmail.com>
2022-08-27 16:00:34 +0300

    adding HOSTCONTEXT

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import NetCard from './Components/Cards/NetCard';
 import { BrowserRouter } from 'react-router-dom'
 import { Nets } from './Components/pages/Nets';
 import { AppRouter } from './Components/AppRouter';
 import NavBar from './Components/NavBar';
+import { HOSTURL } from './types/DataTypes';
 
 
-
+const HostContext = React.createContext(null)
 
 const App = (): JSX.Element => {
+  const [host, setHost] = useState(HOSTURL.LOCALHOST)
   return (
+
+
     <BrowserRouter>
       <NavBar />
       <AppRouter />
