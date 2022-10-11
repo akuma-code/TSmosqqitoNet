87f13d8f700ecbd4c2e9dc7d93d7e44118caf84a
akuma-code <akumapl86@gmail.com>
2022-10-11 19:05:09 +0300

    try glsnodes

--- src/Components/pages/SkladControlPage.tsx
+++ src/Components/pages/SkladControlPage.tsx
@@ -12,3 +12,30 @@ export const SkladControlPage = () => {
         <div>SkladControlPage</div>
     )
 }
+
+
+
+type BalkaType = 'rama' | 'imp' | 'stv-imp' | 'stv-rama'
+type PositionType = 'top' | 'bot' | 'left' | 'right'
+
+class GNode {
+    top: BalkaType
+    bot: BalkaType
+    left: BalkaType
+    right: BalkaType
+    // addNode(pos:PositionType, newNode:GNode):GNode[]
+    constructor(top: BalkaType, bot: BalkaType, left: BalkaType, right: BalkaType) {
+        this.top = top
+        this.bot = bot
+        this.left = left
+        this.right = right
+    }
+
+    addNode(pos: PositionType, newNode: GNode) {
+
+    }
+}
+
+function createNode(top: BalkaType = 'rama', bot: BalkaType = 'rama', left: BalkaType = 'rama', right: BalkaType = 'rama') {
+    return new GNode(top, bot, left, right)
+}
