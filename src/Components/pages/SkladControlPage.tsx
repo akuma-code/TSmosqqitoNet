1418a688640f7e8d3989596fe10ad5ec0ccb1bad
akuma-code <akumapl86@gmail.com>
2022-10-12 19:06:06 +0300

    nodes

--- src/Components/pages/SkladControlPage.tsx
+++ src/Components/pages/SkladControlPage.tsx
@@ -13,29 +13,76 @@ export const SkladControlPage = () => {
     )
 }
 
+type ISide = 'top' | 'bot' | 'left' | 'right'
 
 
 type BalkaType = 'rama' | 'imp' | 'stv-imp' | 'stv-rama'
-type PositionType = 'top' | 'bot' | 'left' | 'right'
 
-class GNode {
-    top: BalkaType
-    bot: BalkaType
-    left: BalkaType
-    right: BalkaType
-    // addNode(pos:PositionType, newNode:GNode):GNode[]
-    constructor(top: BalkaType, bot: BalkaType, left: BalkaType, right: BalkaType) {
-        this.top = top
-        this.bot = bot
-        this.left = left
-        this.right = right
+
+
+interface INodeSides {
+    top: Balka,
+    bot: Balka,
+    left: Balka,
+    right: Balka
+}
+class Balka {
+    type: BalkaType
+    constructor(type: BalkaType) {
+        this.type = type
+    }
+    convert(): BalkaType {
+
+        if (this.type === 'rama') return 'imp'
+        if (this.type === 'stv-rama') return 'stv-imp'
+        return this.type
     }
 
-    addNode(pos: PositionType, newNode: GNode) {
+}
 
+class Node {
+    top: Balka
+    bot: Balka
+    left: Balka
+    right: Balka
+    constructor(sides: INodeSides) {
+        this.top = sides.top
+        this.bot = sides.bot
+        this.left = sides.left
+        this.right = sides.right
+    }
+
+    get info() {
+        const info = { top: this.top, left: this.left, bot: this.bot, right: this.right }
+        return info
+    }
+
+    connect(side: ISide) {
+        return this[side].convert()
     }
 }
 
-function createNode(top: BalkaType = 'rama', bot: BalkaType = 'rama', left: BalkaType = 'rama', right: BalkaType = 'rama') {
-    return new GNode(top, bot, left, right)
+class NodeFactory {
+    makeRamaNode() {
+        const top = new Balka('rama')
+        const bot = new Balka('rama')
+        const left = new Balka('rama')
+        const right = new Balka('rama')
+        return new Node({ top, bot, left, right })
+    }
+
+    newNode(topB: BalkaType, botB: BalkaType, leftB: BalkaType, rightB: BalkaType) {
+        const top = new Balka(topB)
+        const bot = new Balka(botB)
+        const left = new Balka(leftB)
+        const right = new Balka(rightB)
+        return new Node({ top, bot, left, right })
+    }
+
+    addNode(baseNode: Node, newNode: Node, side: ISide = 'right') {
+        const CONSTRUCT = []
+
+        CONSTRUCT.push(baseNode)
+
+    }
 }
