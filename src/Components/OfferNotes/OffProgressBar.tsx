a5b68010a4d4ebfd7377c0d98a0ffed3a39926f6
akuma-code <akumapl86@gmail.com>
2023-02-01 14:48:57 +0300

    sync

--- src/Components/OfferNotes/OffProgressBar.tsx
+++ src/Components/OfferNotes/OffProgressBar.tsx
@@ -1,12 +1,13 @@
 import { Button, IconButton } from '@chakra-ui/button'
 import { Box, Text } from '@chakra-ui/layout'
 import { Progress } from '@chakra-ui/progress'
-import { Checkbox, CheckboxGroup, CheckboxIcon, Tooltip } from '@chakra-ui/react'
+import { Checkbox, CheckboxGroup, CheckboxIcon, Tooltip, useCheckbox } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { BsFillCheckCircleFill } from 'react-icons/bs'
 import { BiChevronDownCircle, BiChevronRightCircle, BiCircle } from 'react-icons/bi'
 import { useID } from '../../hooks/useID'
 import { checkboxTheme } from './CbxTheme'
+import { Icon } from '@chakra-ui/icon'
 type OffProgressBarProps = {
     children?: React.ReactNode
     steps: string[]
@@ -16,6 +17,7 @@ export const OffProgressBar: React.FC<OffProgressBarProps> = ({ children, steps 
     const _id = useID
     const stepCount = steps.length
     const [stepsState, setStepsState] = useState(steps.map(s => ({ isChecked: false, text: s, id: _id() })))
+    const { getCheckboxProps, getLabelProps, htmlProps } = useCheckbox()
     return (
         <Box
             bgColor={'red.400'}
@@ -38,7 +40,9 @@ export const OffProgressBar: React.FC<OffProgressBarProps> = ({ children, steps 
                             hasArrow
                             arrowSize={15}
                         >
-                            <Checkbox
+                            <Checkbox {...htmlProps}
+
+
                                 // icon={ss.isChecked ? <BiChevronDownCircle size={30} /> : <BiCircle size={30} />}
                                 isChecked={ss.isChecked}
                                 onChange={() => setStepsState(prev => prev.map(st => st.id === ss.id ? ({ ...st, isChecked: !st.isChecked }) : st))}
@@ -46,7 +50,7 @@ export const OffProgressBar: React.FC<OffProgressBarProps> = ({ children, steps 
                                 isIndeterminate={true}
 
                             >
-                                <CheckboxIcon as={BiChevronDownCircle} />
+                                <Icon  {...getLabelProps()} ><BiChevronDownCircle size={36} /></Icon>
                             </Checkbox>
                         </Tooltip>
                     )}
